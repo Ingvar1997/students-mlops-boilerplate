7e8afa396016c31947ad4752355f3ffbdb85eed4
Ingvar1997 <igor.balykin.bsu@gmail.com>
2021-09-21 20:14:50 +0300

    Added tester and helpers code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from helpers import convert_base64_im_to_np, convert_np_im_to_base64
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -22,3 +24,15 @@ def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+@app.route('/v1.0/image/run-segmentation', methods=['POST'])
+def run_segmentation():
+    if not request.json or not 'im_base64' in request.json:
+        return jsonify({'error_msg': 'No image provided'}, 400)
+    request_json = request.json  
+    im_base64 = request_json['im_base64']
+    im_np = convert_base64_im_to_np(im_base64)
+
+    # DO SOME MANIPULATIONS HERE
+
+    transformed_image = convert_np_im_to_base64(im_np)
+    return jsonify({'im_base64': transformed_image}), 200
